46ee62c7ca109dc27e146fce64bc059111885c21
Peace Adekoya <124872175+CertifiedDaydreamer@users.noreply.github.com>
2023-06-15 10:10:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -161,5 +161,150 @@ form.addEventListener("submit", (e) => {
   card_1.classList.add("hide");
   card_2.classList.remove("hide");
 });
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
 
 ==*/
